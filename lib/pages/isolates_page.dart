1bbcc654febe15abc553f50cbfd680a8b6841a27
Alexander <160505324+alexanderhertsyk@users.noreply.github.com>
2024-04-16 13:18:02 +0300

    Refactored Isolates page.

--- lib/pages/isolates_page.dart
+++ lib/pages/isolates_page.dart
@@ -16,8 +16,8 @@ class IsolatesPage extends StatefulWidget {
 }
 
 class _IsolatesPageState extends State<IsolatesPage> {
-  List<NewsModel> data = <NewsModel>[];
-  bool get isLoading => data.isEmpty;
+  List<NewsModel> news = <NewsModel>[];
+  bool get isLoading => news.isEmpty;
 
   @override
   void initState() {
@@ -27,51 +27,39 @@ class _IsolatesPageState extends State<IsolatesPage> {
   }
 
   Future<void> loadData() async {
-    final receivePort = ReceivePort();
-    await Isolate.spawn(dataLoader, receivePort.sendPort);
-
-    // The 'echo' isolate sends its SendPort as the first message
-    final sendPort = await receivePort.first as SendPort;
-    final msg = await sendReceive(
-      sendPort,
+    final initialReceiver = ReceivePort();
+    await Isolate.spawn<SendPort>(dataLoader, initialReceiver.sendPort);
+    var isolateSender = await initialReceiver.first as SendPort;
+    var mainReceiver = ReceivePort();
+    isolateSender.send(<dynamic>[
       'https://jsonplaceholder.typicode.com/posts',
-    );
-    setState(() {
-      data = msg;
-    });
+      mainReceiver.sendPort,
+    ]);
+    var news = await mainReceiver.first as List<NewsModel>;
+    print(news.length);
+    setState(() => this.news = news);
   }
 
-// The entry point for the isolate
-  static Future<void> dataLoader(SendPort sendPort) async {
-    // Open the ReceivePort for incoming messages.
-    final port = ReceivePort();
-    // Notify any other isolates what port this isolate listens to.
-    sendPort.send(port.sendPort);
-    await for (final dynamic msg in port) {
-      final url = msg[0] as String;
-      final replyTo = msg[1] as SendPort;
+  static Future<void> dataLoader(SendPort initialSender) async {
+    final isolatedPort = ReceivePort();
+    initialSender.send(isolatedPort.sendPort);
 
+    await for (final dynamic msg in isolatedPort) {
+      final mainSender = msg[1] as SendPort;
+      final url = msg[0] as String;
       final Uri dataURL = Uri.parse(url);
       final http.Response response = await http.get(dataURL);
-      // Lots of JSON to parse
-      // replyTo.send(jsonDecode(response.body) as List<Map<String, dynamic>>);
       List<NewsModel> news = List.empty();
+
+      print(response.statusCode);
 
       if (response.statusCode == 200) {
         var newsList = jsonDecode(response.body) as List;
         news = newsList.map((json) => NewsModel.fromJson(json)).toList();
       }
 
-      replyTo.send(news);
+      mainSender.send(news);
     }
-  }
-
-  Future<List<NewsModel>> sendReceive(SendPort port, String msg) async {
-    final ReceivePort response = ReceivePort();
-    port.send(<dynamic>[msg, response.sendPort]);
-
-    var a = await response.first;
-    return a as List<NewsModel>;
   }
 
   Widget getBody() => isLoading ? getLoadingIndicator() : getListView();
@@ -83,7 +71,7 @@ class _IsolatesPageState extends State<IsolatesPage> {
     return ListView.builder(
       itemBuilder: (context, i) => Padding(
         padding: const EdgeInsets.all(10.0),
-        child: Text('Row ${data[i].title}'),
+        child: Text('Row ${news[i].title}'),
       ),
     );
   }
