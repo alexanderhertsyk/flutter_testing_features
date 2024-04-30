ea11280c774c2c6347209e23bb954c03adbee54a
Alexander <160505324+alexanderhertsyk@users.noreply.github.com>
2024-04-30 16:42:47 +0300

    Added check result logic: - Added possible line list; - Added conversion that list into map, where each game field has a list of possible lines to check; - Disabled input for busy cell or after game over; - Added the highlighting for the win line.

--- lib/models/sign.dart
+++ lib/models/sign.dart
@@ -2,6 +2,12 @@ class Sign {
   final bool isCross;
   // TODO: replace w/ enum?
   final int order;
+  final bool isWin;
 
-  Sign({required this.isCross, required this.order});
+  Sign({required this.isCross, required this.order, this.isWin = false});
+
+  Sign.win(Sign sign)
+      : isCross = sign.isCross,
+        order = sign.order,
+        isWin = true;
 }
--- lib/pages/game_page.dart
+++ lib/pages/game_page.dart
@@ -15,8 +15,10 @@ class _GamePageState extends State<GamePage> {
   static const int length = 3;
   static const int size = length * length;
 
+  final List<List<List<int>>> winLines = List.generate(size, (i) => []);
   late List<Sign?> signs;
   int move = 0;
+  bool gameOver = false;
 
   List<Sign?> get initSigns =>
       List.generate(size, (_) => null, growable: false);
@@ -25,33 +27,78 @@ class _GamePageState extends State<GamePage> {
   void initState() {
     super.initState();
 
+    setupWinLines();
     signs = initSigns;
+  }
+
+  void setupWinLines() {
+    List<List<int>> checkLines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (int i = 0; i < size; i++) {
+      for (int j = 0; j < checkLines.length; j++) {
+        if (checkLines[j].contains(i)) {
+          winLines[i].add(checkLines[j]);
+        }
+      }
+    }
   }
 
   void reset() {
     move = 0;
     setState(() => signs = initSigns);
+    gameOver = false;
   }
 
-  bool checkResult() {
-    return true;
-  }
+  List<int>? checkWin(int i, bool? isCross) {
+    for (var line in winLines[i]) {
+      if (line.every((x) => signs[x]?.isCross == isCross)) {
+        return line;
+      }
+    }
 
-  void resetOldValues() {}
+    return null;
+  }
 
   void onTap(int i) {
-    if (signs[i] != null) return;
+    if (gameOver || signs[i] != null) return;
 
     move++;
-    bool isCross = move % 2 != 0;
-
+    bool isCross = move % 2 == 1;
     setState(() => signs[i] = Sign(isCross: isCross, order: 0));
+
+    var winLine = checkWin(i, isCross);
+
+    if (winLine != null) {
+      gameOver = true;
+      highlightWinLine(winLine);
+    }
+  }
+
+  void highlightWinLine(List<int> winLine) {
+    for (var i in winLine) {
+      setState(() => signs[i] = Sign.win(signs[i]!));
+    }
   }
 
   IconData? getIcon(Sign? sign) {
     if (sign == null) return null;
 
     return sign.isCross ? Icons.close : Icons.circle_outlined;
+  }
+
+  Color? getColor(Sign? sign) {
+    if (sign == null) return null;
+
+    return sign.isWin ? Colors.green : Colors.black;
   }
 
   Widget getField(int i) {
@@ -64,7 +111,11 @@ class _GamePageState extends State<GamePage> {
             width: 0.5,
           ),
         ),
-        child: Icon(getIcon(signs[i])),
+        child: Icon(
+          getIcon(signs[i]),
+          color: getColor(signs[i]),
+          size: 100,
+        ),
       ),
     );
   }
